0858d7379904213098354407c935cc5818f6ec2f
Christopher Keil <ckeil0689@gmail.com>
2017-02-10 17:30:55 +0100

    changing edge list generation to use data.table package for speed

--- scripts/create_interactions.r
+++ scripts/create_interactions.r
@@ -1,5 +1,16 @@
 #!/usr/bin/env Rscript
 # Takes a combined matrix file and transforms it to a list of node-node interactions that can be loaded into Cytoscape
+print("Checking for data.table package.")
+list.of.packages <- c("data.table")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+# install if missing
+if(length(new.packages)) {
+  print("Installing data.table package...")
+  install.packages(new.packages, repos="http://cran.rstudio.com/")
+}
+
+library(data.table)
+
 setwd(paste0(getwd(), "/../suppl/data/analysis/"))
 
 args <- commandArgs(trailingOnly=TRUE)
@@ -22,17 +33,28 @@ if(!file.exists(matpath)) {
 }
 
 # read in the combined matrix file
+print(paste("Reading matrix:", matpath))
 cst <- as.data.frame(read.table(matpath, sep="\t", header=TRUE))
 rownames(cst) <- cst[, 1]
 cst[, 1] <- NULL
 
-edges <- NULL
+print("Transforming matrix to node-node-value list.")
+edge_num <- nrow(cst) * ncol(cst)
+print(paste("Edges to write:", edge_num))
+
+#pre-allocate data table since dimensions are known
+edges <- data.table("node1"=rep(NA,nrow(cst)), "node2"=rep(NA,nrow(cst)), "value"==rep(0,nrow(cst)))
+filename <- paste0(combo, "_edges.txt")
+write.table(edges, file = filename, row.names=FALSE, quote=FALSE, sep='\t')
+stop()
+
 for (i in 1:nrow(cst)) {
+  if(i%%100==0) cat("\r", paste0("Progress: ", round((i*100/nrow(cst)), digits = 0), "%"))
   for (j in 1:ncol(cst)) {
-    edges <- rbind(edges, c(rownames(cst)[i], rownames(cst)[j], cst[i,j]))
+    edges <- rbind(edges, c(rownames(cst)[i], colnames(cst)[j], cst[i,j]))
   }
 }
-
+print("Done. Writing file...")
 colnames(edges) <- c('node1', 'node2', 'value')
 filename <- paste0(combo, "_edges.txt")
 write.table(edges, file = filename, row.names=FALSE, quote=FALSE, sep='\t')
