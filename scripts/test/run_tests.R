c7a70db6ad9c473611a58678950af33fd709b298
Christopher Keil <ckeil0689@gmail.com>
2017-03-21 10:53:54 +0100

    added install procedure for testthat library

--- scripts/test/run_tests.R
+++ scripts/test/run_tests.R
@@ -1,6 +1,15 @@
 #!/usr/bin/env Rscript
 
 # Test script for unit testing the single functions & components used in the project
+# Perform some intial setups
+list.of.packages <- c("testthat")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+# install if missing
+if(length(new.packages)) {
+  print("Installing data.table package...")
+  install.packages(new.packages, repos="http://cran.rstudio.com/")
+}
+
 library(testthat) 
 
 # Test directory paths
