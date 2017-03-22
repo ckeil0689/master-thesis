957d5d2da32303cec832a27a85be495bfe5da995
Christopher Keil <ckeil0689@gmail.com>
2017-03-22 18:16:21 +0100

    expanded tests to include z-score reading

--- scripts/test/test-util.R
+++ scripts/test/test-util.R
@@ -19,23 +19,45 @@ test_that("writ.mat writes tab-delimited matrix file", {
   outpath <- paste0(getwd(), "/")
   prefix <- "tmp"
   suffix <- "matrix"
+  filename <- "tmp_matrix.txt"
   
   # make sure we are not verifying the creation of a file that already exists
-  expect_that(file.exists("tmp_matrix.txt"), is_false())
+  expect_that(file.exists(filename), is_false())
   # now write that file
-  write.mat(mat, outpath, prefix, suffix)
+  expect_that(write.mat(mat, outpath, prefix, suffix), prints_text(paste("Writing matrix to file:", paste0(outpath, filename))))
   # was it written?
-  expect_that(file.exists("tmp_matrix.txt"), is_true())
+  expect_that(file.exists(filename), is_true())
   
   # load it again
-  loaded_mat <- as.matrix(read.table("tmp_matrix.txt", header = TRUE, sep = "\t", row.names = 1))
+  loaded_mat <- as.matrix(read.table(filename, header = TRUE, sep = "\t", row.names = 1))
   
   # compare
   expect_that(colnames(loaded_mat), equals(cnms))
   expect_that(rownames(loaded_mat), equals(rnms))
   expect_that(loaded_mat, equals(mat))
   
+  # test some problematic input
+  expect_that(write.mat(outpath, prefix, suffix), throws_error())
+  expect_that(write.mat(mat, prefix, suffix), throws_error())
+  expect_that(write.mat(mat, outpath, suffix), throws_error())
+  expect_that(write.mat(mat, outpath, prefix), throws_error())
+  expect_that(write.mat(), throws_error())
+  expect_that(write.mat(outpath, prefix, suffix, mat), throws_error())
+  
   # delete tmp file
-  file.remove("tmp_matrix.txt")
-  expect_that(file.exists("tmp_matrix.txt"), is_false())
+  file.remove(filename)
+  expect_that(file.exists(filename), is_false())
+})
+
+# Loading z-score table mmc5
+test_that("zscores are successfully loaded", {
+  zscores.path <- paste0(getwd(), "/../../suppl/mmc5.xls" )
+  expect_that(file.exists(zscores.path), is_true())
+  
+  # Load the scores using the function
+  zscores.all <- load.zscores(zscores.path)
+  
+  # Check if the scores are as expected
+  expect_that(zscores.all, is_a("matrix"))
+  expect_that(colnames(zscores.all), matches(c("Th17_vs_Th0_Zscores")))
 })
