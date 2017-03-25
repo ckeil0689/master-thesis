b806d245b46ae83c27c83f8fbb8d7819de256353
Christopher Keil <ckeil0689@gmail.com>
2017-03-25 20:34:10 +0100

    extended testing for loading chip data

--- scripts/test/test-chipload.R
+++ scripts/test/test-chipload.R
@@ -3,14 +3,47 @@
 # Testing ChIP score matrix generation
 source(paste0(getwd(), "/../" , "chipExtract-fun.R"))
 
+CORE_TFS <- c("batf", "irf4", "stat3", "maf", "rorc", "fosl2", "hif1a")
+
+# Experiment reference file from Cell paper website
+reflibfile <- paste0(getwd(), "/../../suppl/mmc4.csv")
+if(!file.exists(reflibfile)) {stop("Reference file does not exist, cannot load ChIP-files.")}
+ref.table <- read.table(reflibfile, sep=",", header=TRUE)
+
 context("Fixing TF names")
+
 test_that("TF names are fixed to match conventional output (matching Cell authors example matrices)", {
+  expect_that(fix.tf.name("I_am_TF-GAMMA"), equals("iamtfgamma"))
+  expect_that(fix.tf.name("RoRG"), equals("rorc"))
+  expect_that(fix.tf.name("c-mAF"), equals("maf"))
+  expect_that(fix.tf.name(""), equals(""))
+  expect_that(fix.tf.name("----__-"), equals(""))
+})
+
+context("Extracting information from reference table")
+
+test_that("TF names are extracted correctly from reference table", {
   
-  tf1 <- "I_am_TF-GAMMA"
-  tf2 <- "RoRG"
-  tf3 <- "c-mAF"
+  # Files known as TF-Thx experiment
+  rorc_th0 <- "GSM1004853_SL3779_SL3778_genes.txt"
+  maf_th0 <- "GSM1004798_SL4424_SL4425_genes.txt"
+  batf_th17 <- "GSM1004787_SL3037_SL3036_genes.txt"
+  stat3_th17 <- "GSM1004865_SL3315_SL3319_genes.txt"
+  p300_th0 <- "GSM1004842_SL1948_SL1947_genes.txt"
+  p300_th17 <- "GSM1004851_SL3594_SL3592_genes.txt"
   
-  expect_that(fix.tf.name(tf1), equals("iamtfgamma"))
-  expect_that(fix.tf.name(tf2), equals("rorc"))
-  expect_that(fix.tf.name(tf3), equals("maf"))
+  non_existent <- "GSM0000000_SL00000_SL00000_genes.txt"
+  gibberish <- "ceverwrgwegrfr.txt"
+  empty <- ""
+  
+  expect_that(extract.tf.from.ref(rorc_th0, ref.table, boost.p300 = TRUE, CORE_TFS), is_identical_to("rorc-th0"))
+  expect_that(extract.tf.from.ref(maf_th0, ref.table, boost.p300 = TRUE, CORE_TFS), is_identical_to("maf-th0"))
+  expect_that(extract.tf.from.ref(batf_th17, ref.table, boost.p300 = TRUE, CORE_TFS), is_identical_to("batf-th17"))
+  expect_that(extract.tf.from.ref(stat3_th17, ref.table, boost.p300 = TRUE, CORE_TFS), is_identical_to("stat3-th17"))
+  expect_that(extract.tf.from.ref(p300_th0, ref.table, boost.p300 = TRUE, CORE_TFS), is_identical_to("p300-th0"))
+  expect_that(extract.tf.from.ref(p300_th17, ref.table, boost.p300 = TRUE, CORE_TFS), is_identical_to("p300-th17"))
+  expect_that(extract.tf.from.ref(non_existent, ref.table, boost.p300 = TRUE, CORE_TFS), throws_error("No library match found."))
+  expect_that(extract.tf.from.ref(gibberish, ref.table, boost.p300 = TRUE, CORE_TFS), throws_error("No library match found."))
+  expect_that(extract.tf.from.ref(empty, ref.table, boost.p300 = TRUE, CORE_TFS), throws_error("No library match found."))
+  expect_that(extract.tf.from.ref(NULL, ref.table, boost.p300 = TRUE, CORE_TFS), throws_error("No experiment passed. Stopping."))
 })
