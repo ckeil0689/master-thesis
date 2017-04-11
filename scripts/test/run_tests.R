72d306d62f8a6072cdd3ba412cf03f72f8836330
Christopher Keil <ckeil0689@gmail.com>
2017-04-11 16:01:20 +0200

    added ability to turn on verbose testing

--- scripts/test/run_tests.R
+++ scripts/test/run_tests.R
@@ -3,6 +3,12 @@
 # Test script for unit testing the single functions & components used in the project
 source(paste0(getwd(), "/../addLibraries.R"))
 
+quiet <- TRUE
+args = commandArgs(trailingOnly=TRUE)
+if(length(args) == 1 && args[1] == "verbose") {
+  quiet <- FALSE
+}
+
 # Test directory paths
 test.dir <- getwd()
 results.out <- paste0(test.dir, "/results/")
@@ -13,7 +19,7 @@ if(!dir.exists(results.out)) {
 # Run the tests
 source(paste0(getwd(), "/../setGlobalVars.R"))
 GLOBAL[["DEBUG"]] <- FALSE # turn off for testing if it is enabled in setGlobalVars.R
-GLOBAL[["TEST"]] <- TRUE # turn off for testing if it is enabled in setGlobalVars.R
+GLOBAL[["TEST"]] <- quiet # if TRUE, no output from functions will be printed in addition to testthat output
 source(paste0(getwd(), "/../util.R")) # for write.mat() & println()
 test_dir(test.dir, reporter="summary")
 
