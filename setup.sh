355333c80e1b6dcd9fe2b89fc81502f92cf1db3c
Christopher Keil <ckeil0689@gmail.com>
2017-04-12 18:44:52 +0200

    changed wget layout for mmc4

--- setup.sh
+++ setup.sh
@@ -16,11 +16,16 @@ mkdir -p $dataDir
 mkdir -p $dataDir/{chipseq,deseq,inferelator,rnaseq}
 
 # Download mmc4 table (experiment library reference). TODO remove mmc5 and create code to extract it from DESeq files
-if [ ! -f "$supplDir/mmc4.xlsx" ]; 
-then 
-  echo "Downloading experiment library reference table (mmc4.xlsx) from Cell."
-  wget --directory-prefix=$supplDir http://www.cell.com/cms/attachment/2007961119/2030652145/mmc4.xlsx 
-else
+echo "Checking experiment library reference table (mmc4.xlsx) from Cell."
+wget -nc --directory-prefix=$supplDir http://www.cell.com/cms/attachment/2007961119/2030652145/mmc4.xlsx
+if [ $? -eq 0 ];
+then
   echo "Experiment library reference table (mmc4.xlsx) is present."
+else 
+  echo "Problem when attempting to download experiment library reference table (mmc4.xlsx). Stopping."
+  exit 1
 fi
+
+echo "Web-spidering around to check the presence of GSE40918 on NCBI GEO."
+wget --spider https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE40918&format=file
 echo "Done! All set up and ready to go. To generate a network with the files present in /data/, move into /scripts/ and run 'master.R'"
