17b02df622d417284b69dea28f8051cb1866e0d1
Christopher Keil <ckeil0689@gmail.com>
2017-04-12 18:30:45 +0200

    setup now downloads mmc4.xlsx if not present

--- setup.sh
+++ setup.sh
@@ -13,7 +13,14 @@ parentDir=$(pwd)
 supplDir="$parentDir/suppl"
 dataDir="$supplDir/data"
 mkdir -p $dataDir
-mkdir -p $dataDir/{chipseq, deseq, inferelator, rnaseq}
+mkdir -p $dataDir/{chipseq,deseq,inferelator,rnaseq}
 
-
+# Download mmc4 table (experiment library reference). TODO remove mmc5 and create code to extract it from DESeq files
+if [ ! -f "$supplDir/mmc4.xlsx" ]; 
+then 
+  echo "Downloading experiment library reference table (mmc4.xlsx) from Cell."
+  wget --directory-prefix=$supplDir http://www.cell.com/cms/attachment/2007961119/2030652145/mmc4.xlsx 
+else
+  echo "Experiment library reference table (mmc4.xlsx) is present."
+fi
 echo "Done! All set up and ready to go. To generate a network with the files present in /data/, move into /scripts/ and run 'master.R'"
