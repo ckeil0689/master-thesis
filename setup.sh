5cd4dd6c532045bc746228b6bdf12f476467ad81
Christopher Keil <ckeil0689@gmail.com>
2017-04-12 18:01:21 +0200

    changed sub-dir creation in setup file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-echo "Ensuring all required data is present."
-parentDir=$(pwd)
-supplDir="$parentDir/suppl"
-if [ -d $supplDir ]
+
+echo "Ensuring all required data from NCBI GEO GSE40918 is present."
+# Check if in correct environment (setup.sh could have been moved)
+if [ ! `git rev-parse --git-dir` ];
 then
-  echo "'suppl' subdirectory exists."
-else
-  echo "$supplDir not found." 
-  $(mkdir suppl)
+  echo "Not in git repository."
+  exit 1
 fi
 
+# Create all sub-dirs, make no noise if they exist.
+parentDir=$(pwd)
+supplDir="$parentDir/suppl"
+dataDir="$supplDir/data"
+mkdir -p $dataDir
+mkdir -p $dataDir/{chipseq, deseq, inferelator, rnaseq}
 
+
+echo "Done! All set up and ready to go. To generate a network with the files present in /data/, move into /scripts/ and run 'master.R'"
