02900c13610b779193f28e2b33c1537ac68dbd21
Chris Keil <ckeil0689@gmail.com>
2017-04-26 18:47:23 +0200

    cleaned cache

--- scripts/rscripts/run.R
+++
@@ -1,6 +0,0 @@
-# simple wrapper R script to deal with default R naming conventions
-# 
-# Author: crispy
-###############################################################################
-
-source("scripts/main.R")
