db2f849495b04b6c8b8df96a46565346aa5529e1
Chris Keil <ckeil0689@gmail.com>
2017-06-25 17:22:52 +0200

    added cs heatmaps

--- scripts/analysis/analyze-output.R
+++ scripts/analysis/analyze-output.R
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 library(ggplot2)
-library(reshape2)
 library(plyr)
+library(igraph)
+
 # Analysis of Network Generation Output
 scriptdir <- getwd()
 analysis.dir <- paste0(getwd(), "/../../suppl/data/analysis/")
@@ -21,25 +22,25 @@ if(!dir.exists(outpath.results)) {
 }
 
 print("Beginning analysis of latest files.")
-# Order all output by date
+
+# Retrieve latest output files (by POSIX modification time)
 details = file.info(list.files(path = outpath.cyt, pattern = "*.csv", full.names=TRUE))
 details = details[with(details, order(as.POSIXct(mtime))), ]
 files = rownames(details)
 latest.files <- tail(files, 3) # 3 because activator, repressor, single
 
 print("Analyzed files:")
-print(latest.files)
+print(basename(latest.files))
 
-# Analysis loop (one run per file)
 gen.kc.net <- NULL
+# Confidence score density histograms for each file
 for(i in latest.files) {
-  data <- read.csv(i)
+  data <- read.csv(i, stringsAsFactors = FALSE)
   type <- gsub(paste0(outpath.cyt, "/kc_(activator|repressor|single)_.*csv"),'\\1', i)
   if(type == "single") {
     gen.kc.net <- data
   }
   scores <- data[,"confidence_score"]
-  # Histogram
   scores.hist <- hist(abs(scores), main = paste("KC score density distribution for", type), 
                       freq = FALSE, col = "blue", xlab = "abs(confidence_score)", ylim = range(c(0:3)))
   png(paste0(outpath.results, type, "-scores-hist.png"))
@@ -48,29 +49,34 @@ for(i in latest.files) {
 }
 
 # Barplots for TF - interactions
-tfs <- toupper(c("batf", "irf4", "stat3", "maf", "rorc", "fosl2", "hif1a"))
-
-for(tf in tfs) {
-  entry <- gen.kc.net[which(gen.kc.net[,"nodeA"] == tf),]
-  interactions <- entry[,"interaction"]
-  ggplot(data.frame(interactions), aes(x=interactions, fill=factor(interactions))) + geom_bar(show.legend = FALSE) + scale_fill_manual(breaks=levels(interactions), values=c('red', 'green')) + coord_cartesian(ylim=c(0,1400))
-  ggsave(paste0(tf, "-interaction-barplot-GN.png"))
-}
-
-# Single barplot for TF interactions
 count.ia <- count(gen.kc.net, vars=c("nodeA","interaction"))
 ggplot(count.ia, aes(x = nodeA, y = freq, fill = interaction)) +   
   geom_bar(position = "dodge", stat="identity") + 
-  scale_fill_manual(breaks=levels("interaction"), values=c('green', 'red')) + coord_cartesian(ylim=c(0,1400))
-ggsave("all-interaction-barplot-GN.pdf")
+  scale_fill_manual(breaks=levels("interaction"), values=c('green', 'red')) + 
+  coord_cartesian(ylim=c(0,1400)) + geom_text(aes(label=freq), position=position_dodge(width=0.9), vjust=-0.25) +
+  theme(text = element_text(size=30))
+ggsave("all-interaction-barplot-GN.pdf", width = 16, height = 9)
 
-# Ciofani Network Example (KC)
+# Adjacency heatmaps (latest final matrices from debug directory)
+print("Making interaction heatmaps. This uses the last debug output (enable DEBUG when running master.R).")
+debug.matrices <- list.files(path = outpath.debug, pattern = "kc_signed_(activator|repressor).txt", full.names=TRUE)
+for(i in debug.matrices) {
+  data <- read.csv(i, sep = "\t", row.names = 1, header = TRUE, stringsAsFactors = FALSE)
+  data.m <- data.matrix(data, rownames.force = TRUE)
+  type <- gsub(paste0(outpath.debug, "/kc_signed_(activator|repressor).txt"),'\\1', i)
+  png(paste0(outpath.results, "/", type, "-heatmap-GN.png"))
+  hmap <- heatmap(data.m, Rowv = NA, Colv = NA, labRow = NA, xlab = paste("Heatmap of confidence scores for", type))
+  dev.off()
+}
+
+# --------------------------------------
+# Ciofani Network Example (KC) ---------
+# --------------------------------------
 cio.filename <- paste0(scriptdir, "/kc-edges-formatted.csv")
 cio.kc.net <- NULL
 if(file.exists(cio.filename)) {
   print("Analyzing Ciofani et al. example KC-network for comparison.")
-  cio.kc.net <- read.csv(cio.filename)
-  
+  cio.kc.net <- read.csv(cio.filename, stringsAsFactors = FALSE)
   type <- "single (Ciofani et al.)"
   scores <- cio.kc.net[,"confidence_score"]
   # Histogram
@@ -87,8 +93,10 @@ if(file.exists(cio.filename)) {
 count.ia <- count(cio.kc.net, vars=c("tf","interaction"))
 ggplot(count.ia, aes(x = tf, y = freq, fill = interaction)) +
   geom_bar(position = "dodge", stat="identity") +
-  scale_fill_manual(breaks=levels("interaction"), values=c('green', 'red')) + coord_cartesian(ylim=c(0,1400))
-ggsave("all-interaction-barplot-ciofani.pdf")
+  scale_fill_manual(breaks=levels("interaction"), values=c('green', 'red')) + 
+  coord_cartesian(ylim=c(0,1400)) + geom_text(aes(label=freq), position=position_dodge(width=0.9), vjust=-0.25) +
+  theme(text = element_text(size=30))
+ggsave("all-interaction-barplot-ciofani.pdf", width = 16, height = 9)
 
 # Matrix analysis
 print("Analyzing intermediate matrix files stored in debug folder.")
--- scripts/main/addLibraries.R
+++ scripts/main/addLibraries.R
@@ -2,7 +2,7 @@
 
 # Checks for and if needed installs required R-packages
 # Perform some intial setups
-list.of.packages <- c("data.table", "reshape2", "testthat") #, "xlsx")
+list.of.packages <- c("data.table", "reshape2", "testthat", "ggplot2", "plyr", "igraph") #, "xlsx")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 # install if missing
 if(length(new.packages)) {
@@ -17,5 +17,5 @@ if(length(new.packages)) {
 
 library(data.table)
 library(reshape2)
-library(testthat) 
+library(testthat)
 # library(xlsx) // causes issues with rJava package installs which rely on specific Java dev kit installs --> unreliable mess, very probable to break on different machines
