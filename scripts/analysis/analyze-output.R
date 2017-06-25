bcf85c9e5071ca933810d0f7f62e46c4d926cedd
Chris Keil <ckeil0689@gmail.com>
2017-06-25 02:17:24 +0200

    more barplot stuff

--- scripts/analysis/analyze-output.R
+++ scripts/analysis/analyze-output.R
@@ -1,4 +1,7 @@
 #!/usr/bin/env Rscript
+library(ggplot2)
+library(reshape2)
+library(plyr)
 
 # Analysis of Network Generation Output
 scriptdir <- getwd()
@@ -29,9 +32,13 @@ print("Analyzed files:")
 print(latest.files)
 
 # Analysis loop (one run per file)
+gen.kc.net <- NULL
 for(i in latest.files) {
   data <- read.csv(i)
   type <- gsub(paste0(outpath.cyt, "/kc_(activator|repressor|single)_.*csv"),'\\1', i)
+  if(type == "single") {
+    gen.kc.net <- data
+  }
   scores <- data[,"confidence_score"]
   # Histogram
   scores.hist <- hist(abs(scores), main = paste("KC score density distribution for", type), 
@@ -40,6 +47,19 @@ for(i in latest.files) {
   plot(scores.hist)
   dev.off()
 }
+
+# Barplots for TF - interactions
+tfs <- toupper(c("batf", "irf4", "stat3", "maf", "rorc", "fosl2", "hif1a"))
+
+for(tf in tfs) {
+  entry <- gen.kc.net[which(gen.kc.net[,"nodeA"] == tf),]
+  interactions <- entry[,"interaction"]
+  ggplot(data.frame(interactions), aes(x=interactions, fill=factor(interactions))) + geom_bar(show.legend = FALSE) + scale_fill_manual(breaks=levels(interactions), values=c('red', 'green')) + coord_cartesian(ylim=c(0,1400))
+  ggsave(paste0(tf, "-interaction-barplot-GN.png"))
+}
+
+count.interactions <- count(gen.kc.net, vars=c("nodeA","interaction"))
+print(count.interactions)
 
 # Ciofani Network Example (KC)
 cio.filename <- paste0(scriptdir, "/kc-edges-formatted.csv")
